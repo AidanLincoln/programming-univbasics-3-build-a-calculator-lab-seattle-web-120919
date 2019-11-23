96e4231b9080e8bada56b30bb3475a0bb20041cc
Aidan Lincoln <aidantlincoln@gmail.com>
2019-11-23 22:17:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  return sum
 end
 
 def division(num1, num2)
-
+ sum = num2 / num2
+ return sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  return sum
 end
 
 def modulo(num1, num2)
